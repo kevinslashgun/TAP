fe8da6bf79b203cc8bcfea43f91edea6256c5d58
kevinslashgun <kevinbarragan1402@gmail.com>
2024-04-12 15:16:03 +0200

    finished operators, added ToString, Equal, GetHashCode and tests

--- Fraction/FractionClass/Class1.cs
+++ Fraction/FractionClass/Class1.cs
@@ -1,91 +1,129 @@
-﻿    using System.Runtime.InteropServices;
-    using Microsoft.VisualBasic.CompilerServices;
+﻿using System.Runtime.InteropServices;
+using Microsoft.VisualBasic.CompilerServices;
 
-    namespace FractionClass
+namespace FractionClass
+{
+    public class Fraction
     {
-        public class Fraction
+        public int Numerator { get; } // Numerator can be any integer
+        public int Denominator { get; } // Denominator must be a positive integer
+
+        public Fraction(int numerator, int denominator)
         {
-            public int Numerator { get; } // Numerator can be any integer
-            public int Denominator { get; } // Denominator must be a positive integer
-
-            public Fraction(int numerator, int denominator)
+            if (denominator == 0)
             {
-                if (denominator == 0)
-                {
-                    throw new ArgumentException("Denominator cannot be zero");
-
-                }
-
-                var sign = 1;
-
-                if (numerator == 0) // Any fraction with numerator = 0, will have denominator = 1.
-                {
-                    denominator = 1;
-                }
-                else
-                {
-                    sign = (numerator * denominator) > 0 ? 1 : -1; // Keep track of the sign
-                    // convert numerator and denominator to positive
-                    numerator = Math.Abs(numerator);
-                    denominator = Math.Abs(denominator);
-                    Normalize(ref numerator, ref denominator);
-                }
-
-                this.Numerator = numerator * sign;
-                this.Denominator = denominator;
+                throw new ArgumentException("Denominator cannot be zero");
             }
 
-            public static Fraction operator +(Fraction a, Fraction b)
+            var sign = 1;
+
+            if (numerator == 0) // Any fraction with numerator = 0, will have denominator = 1.
             {
-                // x1/y1+x2/y2=((x1*y2)+(x2*y1))/(y1*y2)
-                return new Fraction((a.Numerator * b.Denominator) + (b.Numerator * a.Denominator),
-                    (a.Denominator * b.Denominator));
+                denominator = 1;
+            }
+            else
+            {
+                sign = (numerator * denominator) > 0 ? 1 : -1; // Keep track of the sign
+                // convert numerator and denominator to positive
+                numerator = Math.Abs(numerator);
+                denominator = Math.Abs(denominator);
+                Normalize(ref numerator, ref denominator);
             }
 
-            public static Fraction operator -(Fraction a, Fraction b)
+            this.Numerator = numerator * sign;
+            this.Denominator = denominator;
+        }
+
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            // x1/y1+x2/y2=((x1*y2)+(x2*y1))/(y1*y2)
+            return new Fraction((a.Numerator * b.Denominator) + (b.Numerator * a.Denominator),
+                (a.Denominator * b.Denominator));
+        }
+
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            // x1/y1-x2/y2=((x1*y2)-(x2*y1))/(y1*y2)
+            return new Fraction((a.Numerator * b.Denominator) - (b.Numerator * a.Denominator),
+                (a.Denominator * b.Denominator));
+        }
+
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            // (x1/y1)*(x2/y2)=(x1*x2)/(y1*y2)
+            return new Fraction((a.Numerator * b.Numerator), (a.Denominator * b.Denominator));
+        }
+
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            // se x2<>0, (x1/y1)/(x2/y2)=(x1*y2)/(y1*x2)
+            if (b.Numerator == 0)
             {
-                // x1/y1-x2/y2=((x1*y2)-(x2*y1))/(y1*y2)
-                return new Fraction((a.Numerator * b.Denominator) - (b.Numerator * a.Denominator),
-                    (a.Denominator * b.Denominator));
+                throw new ArgumentException("Second argument cannot be 0");
             }
+
+            return new Fraction((a.Numerator * b.Denominator), (b.Numerator * a.Denominator));
+        }
 
         private static void Normalize(ref int numerator, ref int denominator)
+        {
+            var gcd = CalculateGcd(numerator, denominator);
+            while (gcd != 1)
             {
-                var gcd = CalculateGcd(numerator, denominator);
-                while (gcd != 1)
-                {
-                    numerator /= gcd;
-                    denominator /= gcd;
-                    gcd = CalculateGcd(numerator, denominator);
-                }
+                numerator /= gcd;
+                denominator /= gcd;
+                gcd = CalculateGcd(numerator, denominator);
+            }
+        }
+
+        private static int
+            CalculateGcd(int numerator, int denominator) // I know for sure that both arguments are positive
+        {
+            /*
+             * Euclidean algorithm
+             * a = Max(numerator, denominator)
+             * b = what's left
+             * find quotient and remainder so that:
+             *  a = quotient * b + remainder
+             * if remainder = 0 then b is the GCD
+             */
+
+            var a = Math.Max(numerator, denominator);
+            var b = Math.Min(numerator, denominator);
+            var remainder = -1;
+
+            while (remainder != 0)
+            {
+                var quotient =
+                    a / b; // Instead of looping until I find the correct quotient, I can just divide and keep the integer
+                remainder = a - (quotient * b);
+                // If remainder != 0 then I have to repeat with the next pair of number (b, remainder)
+                a = b;
+                b = remainder!;
             }
 
-            private static int CalculateGcd(int numerator, int denominator) // I know for sure that both arguments are positive
+            return a;
+        }
+
+        public override string ToString()
+        {
+            if (Denominator == 1)
             {
-                /*
-                 * Euclidean algorithm
-                 * a = Max(numerator, denominator)
-                 * b = what's left
-                 * find quotient and remainder so that:
-                 *  a = quotient * b + remainder
-                 * if remainder = 0 then b is the GCD
-                 */
-
-                var a = Math.Max(numerator, denominator);
-                var b = Math.Min(numerator, denominator);
-                var remainder = -1;
-
-                while (remainder != 0)
-                {
-                    var quotient = a / b; // Instead of looping until I find the correct quotient, I can just divide and keep the integer
-                    remainder = a - (quotient * b);
-                    // If remainder != 0 then I have to repeat with the next pair of number (b, remainder)
-                    a = b;
-                    b = remainder!;
-                }
-
-                return a;
+                return Numerator.ToString();
             }
 
+            return Numerator.ToString() + "/" + Denominator.ToString();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            var other = obj as Fraction;
+            return Numerator.Equals(other.Numerator) && Denominator.Equals(other.Denominator);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Numerator, Denominator).GetHashCode();
         }
     }
+}
--- Fraction/TestFractionClass/UnitTest1.cs
+++ Fraction/TestFractionClass/UnitTest1.cs
@@ -20,7 +20,7 @@ namespace TestFractionClass
         [TestCase(5, 7, 5, 7)]
         [TestCase(7, 5, 7, 5)]
         [TestCase(-1, 2, -1, 2)]
-        [TestCase(-2, 1, -2, 1 )]
+        [TestCase(-2, 1, -2, 1)]
         [TestCase(0, 1, 0, 1)]
         [TestCase(1, -2, -1, 2)]
         public void TestConstructorWithNormalFormFraction(int numerator, int denominator, int expectedNumerator,
@@ -55,10 +55,10 @@ namespace TestFractionClass
             }));
         }
 
-        [TestCase(1, 0)]
-        public void TestConstructorWithInvalidDenominator(int numerator, int denominator)
+        [Test]
+        public void TestConstructorWithInvalidDenominator()
         {
-            Assert.That(() => new Fraction(numerator, denominator), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => new Fraction(1, 0), Throws.TypeOf<ArgumentException>());
         }
 
         [TestCase(1, 1, 1, 1, 2, 1)]
@@ -103,6 +103,91 @@ namespace TestFractionClass
                 Assert.That(retFraction.Numerator, Is.EqualTo(expectedN));
                 Assert.That(retFraction.Denominator, Is.EqualTo(expectedD));
             }));
+        }
+
+        [TestCase(3, 4, 1, 4, 3, 16)]
+        [TestCase(5, 6, 1, 3, 5, 18)]
+        [TestCase(-1, 2, -1, 3, 1, 6)]
+        [TestCase(5, 4, 1, 4, 5, 16)]
+        [TestCase(1, 4, 5, 4, 5, 16)]
+        [TestCase(0, 1, 1, 2, 0, 1)]
+        [TestCase(0, 1, -2, 3, 0, 1)]
+        [TestCase(1, 3, 1, 3, 1, 9)]
+        public void TestMultiplicationOperator(int n1, int d1, int n2, int d2, int expectedN, int expectedD)
+        {
+            _fraction = new Fraction(n1, d1);
+            _fraction2 = new Fraction(n2, d2);
+            var retFraction = _fraction * _fraction2;
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(retFraction.Numerator, Is.EqualTo(expectedN));
+                Assert.That(retFraction.Denominator, Is.EqualTo(expectedD));
+            });
+        }
+
+        [Test]
+        public void TestDivisionOperatorWithInvalidInput()
+        {
+            Assert.That(() => new Fraction(1, 1) / new Fraction(0, 1), Throws.TypeOf<ArgumentException>());
+        }
+
+        [TestCase(3, 4, 1, 4, 3, 1)]
+        [TestCase(5, 6, 1, 3, 5, 2)]
+        [TestCase(-1, 2, -1, 3, 3, 2)]
+        [TestCase(5, 4, 1, 4, 5, 1)]
+        [TestCase(1, 4, 5, 4, 1, 5)]
+        [TestCase(0, 1, 1, 2, 0, 1)]
+        [TestCase(0, 1, -2, 3, 0, 1)]
+        [TestCase(1, 3, 1, 3, 1, 1)]
+        public void TestDivisionOperator(int n1, int d1, int n2, int d2, int expectedN, int expectedD)
+        {
+            // Arrange
+            _fraction = new Fraction(n1, d1);
+            _fraction2 = new Fraction(n2, d2);
+
+            // Act
+            var retFraction = _fraction / _fraction2;
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(retFraction.Numerator, Is.EqualTo(expectedN));
+                Assert.That(retFraction.Denominator, Is.EqualTo(expectedD));
+            });
+        }
+
+        [TestCase(1, 5, "1/5")]
+        [TestCase(5, 1, "5")]
+        [TestCase(-1, 3, "-1/3")]
+        [TestCase(-7, 1, "-7")]
+        [TestCase(0, 1, "0")]
+        [TestCase(0, 10, "0")]
+        public void TestToString(int numerator, int denominator, string expectedString)
+        {
+            Assert.That(() => new Fraction(numerator, denominator).ToString(), Is.EqualTo(expectedString));
+        }
+
+        [TestCase(1,2,2,4,true)]
+        [TestCase(1, 2, 1, 3, false)]
+        [TestCase(-2, 1, -8, 4, true)]
+        [TestCase(-1, 7, -2, 5, false)]
+        [TestCase(0, 1, 0, 1, true)]
+        public void TestEquals(int n1, int d1, int n2, int d2, bool expectedBool)
+        {
+            Assert.That(() => new Fraction(n1, d1).Equals(new Fraction(n2,d2)), Is.EqualTo(expectedBool));
+        }
+
+        [TestCase(2,3)]
+        [TestCase(3, 2)]
+        [TestCase(-2, 5)]
+        [TestCase(-5, 2)]
+        [TestCase(0, 1)]
+        [TestCase(-11, 1)]
+        [TestCase(13,1)]
+        public void TestGetHashCode(int numerator, int denominator)
+        {
+            Assert.That(() => new Fraction(numerator, denominator).GetHashCode(), Is.EqualTo((numerator,denominator).GetHashCode()));
         }
     }
 }
