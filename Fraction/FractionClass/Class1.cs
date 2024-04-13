fd86b0a06c52b83febe69f7875ce95d540d2f705
kevinslashgun <kevinbarragan1402@gmail.com>
2024-04-13 18:16:00 +0200

    Full code cleanup and used ternary expressions

--- Fraction/FractionClass/Class1.cs
+++ Fraction/FractionClass/Class1.cs
@@ -1,127 +1,111 @@
-﻿using System.Runtime.InteropServices;
-using Microsoft.VisualBasic.CompilerServices;
+﻿namespace FractionClass;
 
-namespace FractionClass
+internal class Utilities
 {
-    internal class Utilities
+    internal static int GCD(int a, int b)
     {
-        internal static int GCD(int a, int b)
-        {
-            /*
-             * The logic of this algorithm is to keep replacing the greatest number
-             * with the remainder of the division of itself with the other one
-             * until one becomes zero.
-             *
-             * Example:
-             * a: 8;    b:6;    ->  a = a % b = 8 % 6 = 2
-             * a: 2;    b:6;    ->  b = b % a = 6 % 2 = 0
-             * a: 2;    b:0;    ->  return a | b = 2 | 0 = 2
-             *
-             * If a and b have GCD = 1, they will keep decreasing until one of them becomes 1 and the other one becomes 0
-             */
-            while (a != 0 && b != 0)
-            {
-                if (a > b)
-                {
-                    a %= b;
-                }
-                else
-                {
-                    b %= a;
-                }
-            }
+        /*
+         * The logic of this algorithm is to keep replacing the greatest number
+         * with the remainder of the division of itself with the other one
+         * until one becomes zero.
+         *
+         * Example:
+         * a: 8;    b:6;    ->  a = a % b = 8 % 6 = 2
+         * a: 2;    b:6;    ->  b = b % a = 6 % 2 = 0
+         * a: 2;    b:0;    ->  return a | b = 2 | 0 = 2
+         *
+         * If a and b have GCD = 1, they will keep decreasing until one of them becomes 1 and the other one becomes 0
+         */
+        while (a != 0 && b != 0)
+            if (a > b)
+                a %= b;
+            else
+                b %= a;
 
-            return a | b; // At this point one of these two is equals 0, so I will return the one that isn't zero using bitwise OR operator
-        }
+        return
+            a | b; // At this point one of these two is equals 0, so I will return the one that isn't zero using bitwise OR operator
+    }
+}
+
+public class Fraction
+{
+    public Fraction(int numerator, int denominator)
+    {
+        if (denominator == 0) throw new ArgumentException("Denominator cannot be zero");
+
+        var sign = denominator < 0 ? -1 : 1; // If denominator is negative, I will change sign of both numbers
+        /*
+         * Example:
+         *  n: -1;  d: 1;   s = 1   -> n and d don't change
+         *  n: 1;   d: -1   s = -1  -> n becomes negative and d becomes positive
+         *  n: -1;  d: -1   s = -1  -> n and d becomes positive
+         */
+        var gcd = Utilities.GCD(Math.Abs(numerator), Math.Abs(denominator));
+
+        Numerator = numerator * sign / gcd;
+        Denominator = denominator * sign / gcd;
     }
 
-    public class Fraction
+    public int Numerator { get; } // Numerator can be any integer
+    public int Denominator { get; } // Denominator must be a positive integer
+
+    public static Fraction operator +(Fraction left, Fraction right)
     {
-        public int Numerator { get; } // Numerator can be any integer
-        public int Denominator { get; } // Denominator must be a positive integer
+        // x1/y1+x2/y2=((x1*y2)+(x2*y1))/(y1*y2)
+        return new Fraction(left.Numerator * right.Denominator + right.Numerator * left.Denominator,
+            left.Denominator * right.Denominator);
+    }
 
-        public Fraction(int numerator, int denominator)
-        {
-            if (denominator == 0)
-            {
-                throw new ArgumentException("Denominator cannot be zero");
-            }
+    public static Fraction operator -(Fraction left, Fraction right)
+    {
+        // x1/y1-x2/y2=((x1*y2)-(x2*y1))/(y1*y2)
+        return new Fraction(left.Numerator * right.Denominator - right.Numerator * left.Denominator,
+            left.Denominator * right.Denominator);
+    }
 
-            var sign = denominator < 0 ? -1 : 1; // If denominator is negative, I will change sign of both numbers
-            /*
-             * Example:
-             *  n: -1;  d: 1;   s = 1   -> n and d don't change
-             *  n: 1;   d: -1   s = -1  -> n becomes negative and d becomes positive
-             *  n: -1;  d: -1   s = -1  -> n and d becomes positive
-             */
-            var gcd = Utilities.GCD(Math.Abs(numerator), Math.Abs(denominator));
+    public static Fraction operator *(Fraction left, Fraction right)
+    {
+        // (x1/y1)*(x2/y2)=(x1*x2)/(y1*y2)
+        return new Fraction(left.Numerator * right.Numerator, left.Denominator * right.Denominator);
+    }
 
-            this.Numerator = numerator * sign / gcd;
-            this.Denominator = denominator * sign / gcd;
-        }
+    public static Fraction operator /(Fraction left, Fraction right)
+    {
+        // se x2<>0, (x1/y1)/(x2/y2)=(x1*y2)/(y1*x2)
+        if (right.Numerator == 0) throw new DivideByZeroException("Second argument cannot be 0");
 
-        public static Fraction operator +(Fraction a, Fraction b)
-        {
-            // x1/y1+x2/y2=((x1*y2)+(x2*y1))/(y1*y2)
-            return new Fraction((a.Numerator * b.Denominator) + (b.Numerator * a.Denominator),
-                (a.Denominator * b.Denominator));
-        }
+        return new Fraction(left.Numerator * right.Denominator, right.Numerator * left.Denominator);
+    }
 
-        public static Fraction operator -(Fraction a, Fraction b)
-        {
-            // x1/y1-x2/y2=((x1*y2)-(x2*y1))/(y1*y2)
-            return new Fraction((a.Numerator * b.Denominator) - (b.Numerator * a.Denominator),
-                (a.Denominator * b.Denominator));
-        }
+    public override string ToString()
+    {
+        return Denominator == 1 ? Numerator.ToString() : $"{Numerator}/{Denominator}";
+    }
 
-        public static Fraction operator *(Fraction a, Fraction b)
-        {
-            // (x1/y1)*(x2/y2)=(x1*x2)/(y1*y2)
-            return new Fraction((a.Numerator * b.Numerator), (a.Denominator * b.Denominator));
-        }
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(this, obj)) return true; // If equals is called on the same object
+        if (obj is not Fraction other) return false;
+        return Numerator.Equals(other.Numerator) && Denominator.Equals(other.Denominator);
+    }
 
-        public static Fraction operator /(Fraction a, Fraction b)
-        {
-            // se x2<>0, (x1/y1)/(x2/y2)=(x1*y2)/(y1*x2)
-            if (b.Numerator == 0)
-            {
-                throw new ArgumentException("Second argument cannot be 0");
-            }
+#pragma warning disable IDE0070
+    public override int GetHashCode()
+#pragma warning restore IDE0070
+    {
+        return (Numerator, Denominator).GetHashCode();
+    }
 
-            return new Fraction((a.Numerator * b.Denominator), (b.Numerator * a.Denominator));
-        }
+    public static implicit operator Fraction(int numerator)
+    {
+        return new Fraction(numerator, 1);
+    }
 
-        public override string ToString()
-        {
-            if (Denominator == 1)
-            {
-                return Numerator.ToString();
-            }
-
-            return Numerator.ToString() + "/" + Denominator.ToString();
-        }
-
-        public override bool Equals(object? obj)
-        {
-            if (obj is not Fraction other) return false;
-            return Numerator.Equals(other.Numerator) && Denominator.Equals(other.Denominator);
-        }
-
-        public override int GetHashCode() => (Numerator, Denominator).GetHashCode();
-
-        public static implicit operator Fraction(int numerator)
-        {
-            return new Fraction(numerator, 1);
-        }
-
-        public static explicit operator int(Fraction fraction)
-        {
-            if (fraction.Denominator != 1)
-            {
-                throw new ArgumentException("Fraction denominator must be 1");
-            }
-
-            return fraction.Numerator;
-        }
+    public static explicit operator int(Fraction fraction)
+    {
+        return fraction.Denominator == 1
+            ? fraction.Numerator
+            : throw new InvalidOperationException(
+                $"Cannot convert to int as denominator is {fraction.Denominator}");
     }
 }
--- Fraction/TestFractionClass/UnitTest1.cs
+++ Fraction/TestFractionClass/UnitTest1.cs
@@ -1,240 +1,246 @@
 using FractionClass;
 
-namespace TestFractionClass
+namespace TestFractionClass;
+
+[TestFixture]
+public class Tests
 {
-    [TestFixture]
-    public class Tests
+    [SetUp]
+    public void Setup()
     {
-        private Fraction _fraction = null!;
-        private Fraction _fraction2 = null!;
+    }
 
-        [SetUp]
-        public void Setup()
+    private Fraction _fraction = null!;
+    private Fraction _fraction2 = null!;
+
+    [TestCase(1, 2, 1, 2)]
+    [TestCase(2, 1, 2, 1)]
+    [TestCase(2, 3, 2, 3)]
+    [TestCase(3, 2, 3, 2)]
+    [TestCase(5, 7, 5, 7)]
+    [TestCase(7, 5, 7, 5)]
+    [TestCase(-1, 2, -1, 2)]
+    [TestCase(-2, 1, -2, 1)]
+    [TestCase(0, 1, 0, 1)]
+    [TestCase(1, -2, -1, 2)]
+    [TestCase(1, -1, -1, 1)]
+    public void TestConstructorWithNormalFormFraction(int numerator, int denominator, int expectedNumerator,
+        int expectedDenominator)
+    {
+        _fraction = new Fraction(numerator, denominator);
+
+        Assert.Multiple(() =>
         {
-        }
+            Assert.That(_fraction.Numerator, Is.EqualTo(expectedNumerator));
+            Assert.That(_fraction.Denominator, Is.EqualTo(expectedDenominator));
+        });
+    }
 
-        [TestCase(1, 2, 1, 2)]
-        [TestCase(2, 1, 2, 1)]
-        [TestCase(2, 3, 2, 3)]
-        [TestCase(3, 2, 3, 2)]
-        [TestCase(5, 7, 5, 7)]
-        [TestCase(7, 5, 7, 5)]
-        [TestCase(-1, 2, -1, 2)]
-        [TestCase(-2, 1, -2, 1)]
-        [TestCase(0, 1, 0, 1)]
-        [TestCase(1, -2, -1, 2)]
-        [TestCase(1, -1, -1, 1)]
-        public void TestConstructorWithNormalFormFraction(int numerator, int denominator, int expectedNumerator,
-            int expectedDenominator)
+    [TestCase(2, 4, 1, 2)]
+    [TestCase(4, 8, 1, 2)]
+    [TestCase(9, 3, 3, 1)]
+    [TestCase(3, 9, 1, 3)]
+    [TestCase(1071, 462, 51, 22)]
+    [TestCase(-1071, 462, -51, 22)]
+    [TestCase(0, 10, 0, 1)]
+    [TestCase(1071, -462, -51, 22)]
+    public void TestConstructorWithNotNormalFormFraction(int numerator, int denominator, int expectedNumerator,
+        int expectedDenominator)
+    {
+        _fraction = new Fraction(numerator, denominator);
+
+        Assert.Multiple(() =>
         {
-            _fraction = new Fraction(numerator, denominator);
+            Assert.That(_fraction.Numerator, Is.EqualTo(expectedNumerator));
+            Assert.That(_fraction.Denominator, Is.EqualTo(expectedDenominator));
+        });
+    }
 
-            Assert.Multiple((() =>
-            {
-                Assert.That(_fraction.Numerator, Is.EqualTo(expectedNumerator));
-                Assert.That(_fraction.Denominator, Is.EqualTo(expectedDenominator));
-            }));
-        }
+    [Test]
+    public void TestConstructorWithInvalidDenominator()
+    {
+        Assert.That(() => new Fraction(1, 0), Throws.TypeOf<ArgumentException>());
+    }
 
-        [TestCase(2, 4, 1, 2)]
-        [TestCase(4, 8, 1, 2)]
-        [TestCase(9, 3, 3, 1)]
-        [TestCase(3, 9, 1, 3)]
-        [TestCase(1071, 462, 51, 22)]
-        [TestCase(-1071, 462, -51, 22)]
-        [TestCase(0, 10, 0, 1)]
-        [TestCase(1071, -462, -51, 22)]
-        public void TestConstructorWithNotNormalFormFraction(int numerator, int denominator, int expectedNumerator,
-            int expectedDenominator)
+    [TestCase(1, 1, 1, 1, 2, 1)]
+    [TestCase(1, 2, 1, 2, 1, 1)]
+    [TestCase(-1, 1, -1, 1, -2, 1)]
+    [TestCase(-4, 14, -21, 16, -179, 112)]
+    [TestCase(1, 3, 1, 4, 7, 12)]
+    [TestCase(-1, 2, -1, 3, -5, 6)]
+    [TestCase(3, 5, 2, 5, 1, 1)]
+    [TestCase(0, 1, 1, 2, 1, 2)]
+    [TestCase(1, 1, 2, 1, 3, 1)]
+    [TestCase(1, 3, -1, 3, 0, 1)]
+    [TestCase(1, 2, 2, 5, 9, 10)]
+    public void TestPlusOperator(int n1, int d1, int n2, int d2, int expectedN, int expectedD)
+    {
+        _fraction = new Fraction(n1, d1);
+        _fraction2 = new Fraction(n2, d2);
+        var retFraction = _fraction + _fraction2;
+
+        Assert.Multiple(() =>
         {
-            _fraction = new Fraction(numerator, denominator);
+            Assert.That(retFraction.Numerator, Is.EqualTo(expectedN));
+            Assert.That(retFraction.Denominator, Is.EqualTo(expectedD));
+        });
+    }
 
-            Assert.Multiple((() =>
-            {
-                Assert.That(_fraction.Numerator, Is.EqualTo(expectedNumerator));
-                Assert.That(_fraction.Denominator, Is.EqualTo(expectedDenominator));
-            }));
-        }
+    [TestCase(3, 4, 1, 4, 1, 2)]
+    [TestCase(5, 6, 1, 3, 1, 2)]
+    [TestCase(-1, 2, -1, 3, -1, 6)]
+    [TestCase(5, 4, 1, 4, 1, 1)]
+    [TestCase(1, 4, 5, 4, -1, 1)]
+    [TestCase(0, 1, 1, 2, -1, 2)]
+    [TestCase(0, 1, -2, 3, 2, 3)]
+    [TestCase(1, 3, 1, 3, 0, 1)]
+    [TestCase(4, 1, 33, 7, -5, 7)]
+    public void TestMinusOperator(int n1, int d1, int n2, int d2, int expectedN, int expectedD)
+    {
+        _fraction = new Fraction(n1, d1);
+        _fraction2 = new Fraction(n2, d2);
+        var retFraction = _fraction - _fraction2;
 
-        [Test]
-        public void TestConstructorWithInvalidDenominator()
+        Assert.Multiple(() =>
         {
-            Assert.That(() => new Fraction(1, 0), Throws.TypeOf<ArgumentException>());
-        }
+            Assert.That(retFraction.Numerator, Is.EqualTo(expectedN));
+            Assert.That(retFraction.Denominator, Is.EqualTo(expectedD));
+        });
+    }
 
-        [TestCase(1, 1, 1, 1, 2, 1)]
-        [TestCase(1, 2, 1, 2, 1, 1)]
-        [TestCase(-1, 1, -1, 1, -2, 1)]
-        [TestCase(-4, 14, -21, 16, -179, 112)]
-        [TestCase(1, 3, 1, 4, 7, 12)]
-        [TestCase(-1, 2, -1, 3, -5, 6)]
-        [TestCase(3, 5, 2, 5, 1, 1)]
-        [TestCase(0, 1, 1, 2, 1, 2)]
-        [TestCase(1, 1, 2, 1, 3, 1)]
-        [TestCase(1, 3, -1, 3, 0, 1)]
-        [TestCase(1, 2, 2, 5, 9, 10)]
-        public void TestPlusOperator(int n1, int d1, int n2, int d2, int expectedN, int expectedD)
+    [TestCase(3, 4, 1, 4, 3, 16)]
+    [TestCase(5, 6, 1, 3, 5, 18)]
+    [TestCase(-1, 2, -1, 3, 1, 6)]
+    [TestCase(5, 4, 1, 4, 5, 16)]
+    [TestCase(1, 4, 5, 4, 5, 16)]
+    [TestCase(0, 1, 1, 2, 0, 1)]
+    [TestCase(0, 1, -2, 3, 0, 1)]
+    [TestCase(1, 3, 1, 3, 1, 9)]
+    [TestCase(1, 11, 11, 1, 1, 1)]
+    [TestCase(42, 1, 0, 1, 0, 1)]
+    public void TestMultiplicationOperator(int n1, int d1, int n2, int d2, int expectedN, int expectedD)
+    {
+        _fraction = new Fraction(n1, d1);
+        _fraction2 = new Fraction(n2, d2);
+        var retFraction = _fraction * _fraction2;
+
+        Assert.Multiple(() =>
         {
-            _fraction = new Fraction(n1, d1);
-            _fraction2 = new Fraction(n2, d2);
-            var retFraction = _fraction + _fraction2;
+            Assert.That(retFraction.Numerator, Is.EqualTo(expectedN));
+            Assert.That(retFraction.Denominator, Is.EqualTo(expectedD));
+        });
+    }
 
-            Assert.Multiple((() =>
-            {
-                Assert.That(retFraction.Numerator, Is.EqualTo(expectedN));
-                Assert.That(retFraction.Denominator, Is.EqualTo(expectedD));
-            }));
-        }
+    [Test]
+    public void TestDivisionOperatorWithInvalidInput()
+    {
+        Assert.That(() => new Fraction(1, 1) / new Fraction(0, 1), Throws.TypeOf<DivideByZeroException>());
+    }
 
-        [TestCase(3, 4, 1, 4, 1, 2)]
-        [TestCase(5, 6, 1, 3, 1, 2)]
-        [TestCase(-1, 2, -1, 3, -1, 6)]
-        [TestCase(5, 4, 1, 4, 1, 1)]
-        [TestCase(1, 4, 5, 4, -1, 1)]
-        [TestCase(0, 1, 1, 2, -1, 2)]
-        [TestCase(0, 1, -2, 3, 2, 3)]
-        [TestCase(1, 3, 1, 3, 0, 1)]
-        [TestCase(4, 1, 33, 7, -5, 7)]
-        public void TestMinusOperator(int n1, int d1, int n2, int d2, int expectedN, int expectedD)
+    [TestCase(3, 4, 1, 4, 3, 1)]
+    [TestCase(5, 6, 1, 3, 5, 2)]
+    [TestCase(-1, 2, -1, 3, 3, 2)]
+    [TestCase(5, 4, 1, 4, 5, 1)]
+    [TestCase(1, 4, 5, 4, 1, 5)]
+    [TestCase(0, 1, 1, 2, 0, 1)]
+    [TestCase(0, 1, -2, 3, 0, 1)]
+    [TestCase(1, 3, 1, 3, 1, 1)]
+    [TestCase(33, 42, 111, 8, 44, 777)]
+    public void TestDivisionOperator(int n1, int d1, int n2, int d2, int expectedN, int expectedD)
+    {
+        _fraction = new Fraction(n1, d1);
+        _fraction2 = new Fraction(n2, d2);
+        var retFraction = _fraction / _fraction2;
+
+        Assert.Multiple(() =>
         {
-            _fraction = new Fraction(n1, d1);
-            _fraction2 = new Fraction(n2, d2);
-            var retFraction = _fraction - _fraction2;
+            Assert.That(retFraction.Numerator, Is.EqualTo(expectedN));
+            Assert.That(retFraction.Denominator, Is.EqualTo(expectedD));
+        });
+    }
 
-            Assert.Multiple((() =>
-            {
-                Assert.That(retFraction.Numerator, Is.EqualTo(expectedN));
-                Assert.That(retFraction.Denominator, Is.EqualTo(expectedD));
-            }));
-        }
+    [TestCase(1, 5, "1/5")]
+    [TestCase(5, 1, "5")]
+    [TestCase(-1, 3, "-1/3")]
+    [TestCase(-7, 1, "-7")]
+    [TestCase(0, 1, "0")]
+    [TestCase(0, 10, "0")]
+    [TestCase(11, 5, "11/5")]
+    [TestCase(22, 11, "2")]
+    [TestCase(22, -11, "-2")]
+    public void TestToString(int numerator, int denominator, string expectedString)
+    {
+        Assert.That(() => new Fraction(numerator, denominator).ToString(), Is.EqualTo(expectedString));
+    }
 
-        [TestCase(3, 4, 1, 4, 3, 16)]
-        [TestCase(5, 6, 1, 3, 5, 18)]
-        [TestCase(-1, 2, -1, 3, 1, 6)]
-        [TestCase(5, 4, 1, 4, 5, 16)]
-        [TestCase(1, 4, 5, 4, 5, 16)]
-        [TestCase(0, 1, 1, 2, 0, 1)]
-        [TestCase(0, 1, -2, 3, 0, 1)]
-        [TestCase(1, 3, 1, 3, 1, 9)]
-        [TestCase(1, 11, 11, 1, 1, 1)]
-        [TestCase(42, 1, 0, 1, 0, 1)]
-        public void TestMultiplicationOperator(int n1, int d1, int n2, int d2, int expectedN, int expectedD)
+    [TestCase(1, 2, 2, 4, true)]
+    [TestCase(1, 2, 1, 3, false)]
+    [TestCase(-2, 1, -8, 4, true)]
+    [TestCase(-1, 7, -2, 5, false)]
+    [TestCase(0, 1, 0, 1, true)]
+    [TestCase(0, 1, 0, 22, true)]
+    [TestCase(1, 2, 2, 4, true)]
+    public void TestEqualsNotNull(int n1, int d1, int n2, int d2, bool expectedBool)
+    {
+        Assert.That(() => new Fraction(n1, d1).Equals(new Fraction(n2, d2)), Is.EqualTo(expectedBool));
+    }
+
+    [Test]
+    public void TestEqualsWithNullFraction()
+    {
+        Assert.That(() => new Fraction(1, 1).Equals(null), Is.EqualTo(false));
+    }
+
+    [Test]
+    public void TestEqualsWithSameObj()
+    {
+        Assert.That(() =>
         {
-            _fraction = new Fraction(n1, d1);
-            _fraction2 = new Fraction(n2, d2);
-            var retFraction = _fraction * _fraction2;
+            var fraction = new Fraction(1, 1);
+            // ReSharper disable once EqualExpressionComparison
+            return fraction.Equals(fraction); // Dumb test
+        }, Is.EqualTo(true));
+    }
 
-            Assert.Multiple(() =>
-            {
-                Assert.That(retFraction.Numerator, Is.EqualTo(expectedN));
-                Assert.That(retFraction.Denominator, Is.EqualTo(expectedD));
-            });
-        }
+    [TestCase(2, 3)]
+    [TestCase(3, 2)]
+    [TestCase(-2, 5)]
+    [TestCase(-5, 2)]
+    [TestCase(0, 1)]
+    [TestCase(-11, 1)]
+    [TestCase(13, 1)]
+    public void TestGetHashCode(int numerator, int denominator)
+    {
+        Assert.That(() => new Fraction(numerator, denominator).GetHashCode(),
+            Is.EqualTo((numerator, denominator).GetHashCode()));
+    }
 
-        [Test]
-        public void TestDivisionOperatorWithInvalidInput()
+    [TestCase(5)]
+    [TestCase(0)]
+    [TestCase(-5)]
+    [TestCase(42)]
+    public void TestImplicitCastFromIntToFraction(int toCast)
+    {
+        Assert.That(() =>
         {
-            Assert.That(() => new Fraction(1, 1) / new Fraction(0, 1), Throws.TypeOf<ArgumentException>());
-        }
+            Fraction fraction = toCast;
+            return fraction;
+        }, Is.InstanceOf<Fraction>());
+    }
 
-        [TestCase(3, 4, 1, 4, 3, 1)]
-        [TestCase(5, 6, 1, 3, 5, 2)]
-        [TestCase(-1, 2, -1, 3, 3, 2)]
-        [TestCase(5, 4, 1, 4, 5, 1)]
-        [TestCase(1, 4, 5, 4, 1, 5)]
-        [TestCase(0, 1, 1, 2, 0, 1)]
-        [TestCase(0, 1, -2, 3, 0, 1)]
-        [TestCase(1, 3, 1, 3, 1, 1)]
-        [TestCase(33, 42, 111, 8, 44, 777)]
-        public void TestDivisionOperator(int n1, int d1, int n2, int d2, int expectedN, int expectedD)
-        {
-            // Arrange
-            _fraction = new Fraction(n1, d1);
-            _fraction2 = new Fraction(n2, d2);
+    [TestCase(5, 1, 5)]
+    [TestCase(1, 1, 1)]
+    [TestCase(-7, 1, -7)]
+    [TestCase(0, 1, 0)]
+    [TestCase(42, 1, 42)]
+    public void TestValidExplicitCastFromFractionToInt(int numerator, int denominator, int expectedInt)
+    {
+        Assert.That(() => (int)new Fraction(numerator, denominator), Is.EqualTo(expectedInt));
+    }
 
-            // Act
-            var retFraction = _fraction / _fraction2;
-
-            // Assert
-            Assert.Multiple(() =>
-            {
-                Assert.That(retFraction.Numerator, Is.EqualTo(expectedN));
-                Assert.That(retFraction.Denominator, Is.EqualTo(expectedD));
-            });
-        }
-
-        [TestCase(1, 5, "1/5")]
-        [TestCase(5, 1, "5")]
-        [TestCase(-1, 3, "-1/3")]
-        [TestCase(-7, 1, "-7")]
-        [TestCase(0, 1, "0")]
-        [TestCase(0, 10, "0")]
-        [TestCase(11, 5, "11/5")]
-        [TestCase(22, 11, "2")]
-        [TestCase(22, -11, "-2")]
-        public void TestToString(int numerator, int denominator, string expectedString)
-        {
-            Assert.That(() => new Fraction(numerator, denominator).ToString(), Is.EqualTo(expectedString));
-        }
-
-        [TestCase(1, 2, 2, 4, true)]
-        [TestCase(1, 2, 1, 3, false)]
-        [TestCase(-2, 1, -8, 4, true)]
-        [TestCase(-1, 7, -2, 5, false)]
-        [TestCase(0, 1, 0, 1, true)]
-        [TestCase(0, 1, 0, 22, true)]
-        [TestCase(1, 2, 2, 4, true)]
-        public void TestEqualsNotNull(int n1, int d1, int n2, int d2, bool expectedBool)
-        {
-            Assert.That(() => new Fraction(n1, d1).Equals(new Fraction(n2, d2)), Is.EqualTo(expectedBool));
-        }
-
-        [Test]
-        public void TestEqualsWithNullFraction()
-        {
-            Assert.That(() => new Fraction(1, 1).Equals(null), Is.EqualTo(false));
-        }
-
-        [TestCase(2, 3)]
-        [TestCase(3, 2)]
-        [TestCase(-2, 5)]
-        [TestCase(-5, 2)]
-        [TestCase(0, 1)]
-        [TestCase(-11, 1)]
-        [TestCase(13, 1)]
-        public void TestGetHashCode(int numerator, int denominator)
-        {
-            Assert.That(() => new Fraction(numerator, denominator).GetHashCode(),
-                Is.EqualTo((numerator, denominator).GetHashCode()));
-        }
-
-        [TestCase(5)]
-        [TestCase(0)]
-        [TestCase(-5)]
-        [TestCase(42)]
-        public void TestImplicitCastFromIntToFraction(int toCast)
-        {
-            Assert.That(() =>
-            {
-                Fraction fraction = toCast;
-                return fraction;
-            }, Is.InstanceOf<Fraction>());
-        }
-
-        [TestCase(5, 1, 5)]
-        [TestCase(1, 1, 1)]
-        [TestCase(-7, 1, -7)]
-        [TestCase(0, 1, 0)]
-        [TestCase(42, 1, 42)]
-        public void TestValidExplicitCastFromFractionToInt(int numerator, int denominator, int expectedInt)
-        {
-            Assert.That(() => (int)new Fraction(numerator, denominator), Is.EqualTo(expectedInt));
-        }
-
-        [Test]
-        public void TestInvalidExplicitCastFromFractionToInt()
-        {
-            Assert.That(() => (int)new Fraction(1, 2), Throws.TypeOf<ArgumentException>());
-        }
+    [Test]
+    public void TestInvalidExplicitCastFromFractionToInt()
+    {
+        Assert.That(() => (int)new Fraction(1, 2), Throws.TypeOf<InvalidOperationException>());
     }
 }
